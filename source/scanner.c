b6a9ac17ef70518d2a3b064221efa9c3e85fc800
Peter <xbabka01@stud.fit.vutbr.cz>
2017-10-13 01:06:46 +0200

    pridane polia klucovych slov so scanner.c

--- source/scanner.c
+++ source/scanner.c
@@ -2,6 +2,47 @@
 #include "scanner.h"
 #include <string.h>
 
+const char *key_worlds[] = {
+        "As",
+        "Asc",
+        "Declare",
+        "Dim",
+        "Do",
+        "Double",
+        "Else",
+        "End",
+        "Chr",
+        "Function",
+        "If",
+        "Input",
+        "Integer",
+        "Length",
+        "Loop",
+        "Print",
+        "Return",
+        "Scope",
+        "String",
+        "SubStr",
+        "Then",
+        "While"
+};
+
+const char *rez_key_worlds[] = {
+        "And",
+        "Boolean",
+        "Continue",
+        "Elseif",
+        "Exit",
+        "False",
+        "For",
+        "Next",
+        "Not",
+        "Or",
+        "Shared",
+        "Static",
+        "True"
+};
+
 FILE *file;     //zdrojovy subor
 
 void set_source_file(FILE *f){
