88a103df0a2c3e280180cfe983b61d463fc3f853
Peter <xbabka01@stud.fit.vutbr.cz>
2017-10-28 21:43:16 +0200

    ciastocne urobene testy na scanner

--- tests/scanner_test.cpp
+++ tests/scanner_test.cpp
@@ -1,11 +1,29 @@
 #include "gtest/gtest.h"
 
+
 extern "C"{
 #include "../source/scanner.h"
 }
 
+
 namespace {
-    class scanner_fixture : public ::testing::Test{
+
+    class scanner_fixture_basic_1 : public ::testing::Test{
+    protected:
+
+        virtual void SetUp(){
+            f = fopen("../tests/scanner_test_input/basic_in", "r");
+            if (f == NULL) {
+                printf("NULL\n");
+            }
+        }
+
+        virtual void TearDown(){
+            fclose(f);
+        }
+    };
+
+    class scanner_fixture_basic_2 : public ::testing::Test{
     protected:
 
         virtual void SetUp(){
@@ -19,13 +37,264 @@ namespace {
             fclose(f);
         }
     };
+
+    class scanner_fixture_comment : public ::testing::Test{
+    protected:
+
+        virtual void SetUp(){
+            f = fopen("../tests/scanner_test_input/basic_in_comment", "r");
+            if (f == NULL) {
+                printf("NULL\n");
+            }
+        }
+
+        virtual void TearDown(){
+            fclose(f);
+        }
+    };
+
 }
 
-TEST_F(scanner_fixture, test1){
+TEST_F(scanner_fixture_basic_1, basic_test1){
+    const int size = 15;
 
-    //t_token token = get_token();
+    t_token *tmp;
+    int i = 0;
 
-    EXPECT_TRUE(1==1);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_scope);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_end);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_scope);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EMPTY);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EMPTY);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+
+
+
+}
+
+
+TEST_F(scanner_fixture_basic_2, basic_test2){
+    t_token *tmp;
+    int i = 0;
+
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_scope);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_dim);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, ID);
+    EXPECT_STREQ(tmp->data.s, "a");
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_as);
+
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_string);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EQ);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, STR);
+    EXPECT_STREQ(tmp->data.s, "");
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_end);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_scope);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EMPTY);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EMPTY);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+
+
+
+}
+
+TEST_F(scanner_fixture_comment, comment_test){
+    t_token *tmp = NULL;
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_string);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_scope);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_dim);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, ID);
+    EXPECT_STREQ(tmp->data.s, "a");
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_as);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_string);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EQ);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, STR);
+    EXPECT_STREQ(tmp->data.s, "");
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EOL);
+    EXPECT_TRUE(tmp->data.s == NULL);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_end);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, MIN_KEY_WORLD);
+    EXPECT_EQ(tmp->data.i, k_scope);
+
+    tmp = get_token();
+    EXPECT_EQ(tmp->token_type, EMPTY);
+    EXPECT_TRUE(tmp->data.s == NULL);
 }
 
 int main(int argc, char **argv) {
